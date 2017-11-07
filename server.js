d13833bf7cbfb0e864184ee2c42111965636c95d
Filip Defar <filip.defar@toptal.com>
2017-11-07 16:41:16 +0100

    Fix update

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ var path    = require("path");
 app.use(sslRedirect());
 app.use(express.static(path.join(__dirname+'/public/')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/pages/index.html')))
+app.get('/update.xml', (req, res) => res.sendFile(path.join(__dirname+'/pages/update.xml')))
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
