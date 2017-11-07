9fe1a9489bcb289eb1650c85d8654ce8246f84a7
Filip Defar <filip.defar@toptal.com>
2017-11-07 16:30:09 +0100

    Define port

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ var path    = require("path");
 app.use(express.static(path.join(__dirname+'/public/')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/pages/index.html')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
